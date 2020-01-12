22f66cf3380c7b4ff543475a87a84075a4cd9b53
ALFALUL <svatopluk.kvas@gmail.com>
2020-01-13 00:12:54 +0100

    4 commit

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame as pg
 import random
 from settings import *
 from sprites import *
+from os import path
+vec = pg.math.Vector2
 
 
 class Game:
@@ -13,14 +15,33 @@ class Game:
         pg.display.set_icon(pg.image.load("png/001-sword.png"))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
+        self.pom=0
         self.font_name = pg.font.match_font(FONT_NAME)
         self.running = True
-    
+        self.load_data()
+        
+    def load_data(self):
+        self.dir= path.dirname(__file__)
+        with open(path.join(self.dir, HS_file), 'w') as f:
+            try:
+                self.hightscore = int(f.read())
+            except:
+                self.hightscore = 0
+        self.snd_dir=path.join(self.dir, 'snd')
+        self.laser_sound = pg.mixer.Sound(path.join(self.snd_dir, 'laser7.ogg'))
+        self.exp_sound = pg.mixer.Sound(path.join(self.snd_dir, 'Explosion.ogg'))
     def new(self):
         #new game
-        self.time = 0
+        pg.mixer.music.load("snd/StarWarsCantinaBand.ogg")
+        pg.mixer.music.set_volume(0.09)
+        self.hp = 3
+        self.score = 0
         self.all_sprites = pg.sprite.Group()
         self.platforms = pg.sprite.Group()
+        self.bullets = pg.sprite.Group( )
+        self.mobs = pg.sprite.Group()
+        self.mob_timer =0
+        self.hit_timer =0
         self.player = Player(self)
         self.all_sprites.add(self.player)
         for plat in PLATFORM_LVL1:
@@ -31,17 +52,41 @@ class Game:
 
     def run(self):
         #loop
+        pg.mixer.music.play(loops=-1)
         self.playing = True
         while self.playing:
             self.clock.tick(FPS)
             self.events()
             self.update()
             self.draw()
+        pg.mixer.music.fadeout(500)
 
     def update(self):
         #loop - update
-        
         self.all_sprites.update()
+
+        #mob spawn?
+        now1 = pg.time.get_ticks()
+        if now1-self.mob_timer > MOB_FREQ + random.choice([-500,0,500]):
+            self.mob_timer = now1
+            Mob(self)
+        mob_hits = pg.sprite.spritecollide(self.player,self.mobs,False)
+        now2 = pg.time.get_ticks()
+        if now2-self.hit_timer >1000 and mob_hits:
+            self.hit_timer = now2
+            self.hp -=1
+        bullets_hits = pg.sprite.groupcollide(self.bullets,self.mobs,True,True)
+        if bullets_hits:
+            self.exp_sound.play()
+            self.score +=10
+            self.pom +=10
+        
+        if self.pom==100:
+            if self.hp < 3:
+                self.hp += 1
+            self.pom =0
+
+
         if self.player.vel.y >0:
             hits = pg.sprite.spritecollide(self.player,self.platforms,False)
             if hits:
@@ -56,6 +101,14 @@ class Game:
                     sprites.kill()
             if len(self.platforms)==0:        
                 self.playing = False
+        if self.hp==0:
+            for sprites in self.all_sprites:
+                sprites.rect.y -= max(self.player.vel.y,10)
+                if sprites.rect.bottom < 0:
+                    sprites.kill()
+            if len(self.platforms)==0:        
+                self.playing = False
+
 
     def events(self):
         #loop - event
@@ -67,8 +120,25 @@ class Game:
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_SPACE:
                 self.player.jump() 
+                
             if event.key == pg.K_ESCAPE:
                 self.show_menu_screen()
+            if event.key == pg.K_UP:
+                self.laser_sound.play()
+                bullet = Bulett(self.player.rect.centerx, self.player.rect.top, -10, 1)
+                self.all_sprites.add(bullet)
+                self.bullets.add(bullet)
+            if event.key == pg.K_LEFT:
+                self.laser_sound.play()
+                bullet = Bulett(self.player.rect.centerx, self.player.rect.top, -10, 0)
+                self.all_sprites.add(bullet)
+                self.bullets.add(bullet)
+            if event.key == pg.K_RIGHT:
+                self.laser_sound.play()
+                bullet = Bulett(self.player.rect.centerx, self.player.rect.top, 10, 0)
+                self.all_sprites.add(bullet)
+                self.bullets.add(bullet)
+           
 
 
 
@@ -77,34 +147,60 @@ class Game:
         #loop - draw
         self.screen.fill(BGCOLOR)
         self.all_sprites.draw(self.screen)
-        self.draw_text(str(self.time),40,WHITE,WIDTH/2,HEIGHT/4)
+        self.draw_text(str(self.score),40,WHITE,WIDTH/2,10)
+        self.draw_text(str(self.hp),40,WHITE,WIDTH-65,10)
+        if self.hp == 3:
+            pg.draw.rect(self.screen,PINK,(WIDTH-45,10,40,40))
+        if self.hp == 2:
+            pg.draw.rect(self.screen,PINK,(WIDTH-45,10,40,40))
+            pg.draw.rect(self.screen,BLACK,(WIDTH-45,10,40,20))
+        if self.hp == 1:
+            pg.draw.rect(self.screen,PINK,(WIDTH-45,10,40,40))
+            pg.draw.rect(self.screen,BLACK,(WIDTH-45,10,40,30))
 
         # *after* DRAW 
         pg.display.flip()
 
     def show_start_screen(self):
+        pg.mixer.music.load("snd/sax.ogg")
+        pg.mixer.music.set_volume(0.07)
+        pg.mixer.music.play(loops=-1)
         self.screen.fill(BGCOLOR)
-        self.draw_text("ZULUL",40,WHITE,WIDTH/2,HEIGHT/4)
-        self.draw_text("Press any key to play",30,BLACK,WIDTH/2,HEIGHT/2.25)
-        self.draw_text("Arrows to move, Space to jump",20,BLACK,WIDTH/2,HEIGHT/1.85)
+        self.draw_text("GAME",40,WHITE,WIDTH/2,HEIGHT/4)
+        self.draw_text("Press  space to play",30,BLACK,WIDTH/2,HEIGHT/2.25)
+        self.draw_text("A,D pohyb, Space skok",20,BLACK,WIDTH/2,(HEIGHT/8)*7)
         pg.display.flip()
         self.wait_for_key()
+        pg.mixer.music.fadeout(500)
     
     def show_menu_screen(self):
         self.screen.fill(BGCOLOR)
-        self.draw_text("ZULUL",40,WHITE,WIDTH/2,HEIGHT/4)
+        pg.mixer.music.set_volume(0.03)
+        self.draw_text("GAME MENU",40,WHITE,WIDTH/2,HEIGHT/4)
         self.draw_text("Press space to play",30,BLACK,WIDTH/2,HEIGHT/2.25)
-        self.draw_text("Arrows to move, Space to jump",20,BLACK,WIDTH/2,HEIGHT/1.85)
+        self.draw_text("A,D pohyb, Space skok",20,BLACK,WIDTH/2,(HEIGHT/8)*7)
+        self.draw_text("Score: " +str(self.score),20,BLACK,WIDTH/2,HEIGHT/1.85)
         pg.display.flip()
         self.wait_for_key()
+        pg.mixer.music.set_volume(0.09)
     
     def show_go_screen(self):
         if not self.running:
             return
+        pg.mixer.music.load("snd/You Died.ogg")
+        pg.mixer.music.set_volume(0.07)
+        pg.mixer.music.play(loops=1)
         self.screen.fill(BGCOLOR)
         self.draw_text("GAME OVER",40,WHITE,WIDTH/2,HEIGHT/4)
-        self.draw_text("Press any key to play again",30,BLACK,WIDTH/2,HEIGHT/2.25)
-        self.draw_text("TIME: " +str(self.time),20,BLACK,WIDTH/2,HEIGHT/1.85)
+        self.draw_text("Press space to play again",30,BLACK,WIDTH/2,HEIGHT/2.25)
+        self.draw_text("Score: " +str(self.score),20,BLACK,WIDTH/2,HEIGHT/1.7)
+        if self.score > self.hightscore:
+            self.hightscore = self.score
+            self.draw_text("LMAO, Nove Hight Score", 30,BLACK,WIDTH/2,HEIGHT-60)
+            with open(path.join(self.dir, HS_file),'w') as f:
+                f.write(str(self.hightscore))
+        else:
+            self.draw_text("Hight Score: " +str(self.hightscore),20,BLACK,WIDTH/2,HEIGHT/1.9)
         pg.display.flip()
         self.wait_for_key()
     
--- sprites.py
+++ sprites.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from settings import *
+import random
 vec = pg.math.Vector2
 
 class Player(pg.sprite.Sprite):
@@ -15,17 +16,18 @@ class Player(pg.sprite.Sprite):
         self.acc = vec(0,0)
     
     def jump(self):
-        
         hits = pg.sprite.spritecollide(self,self.game.platforms, False)
         if hits:
             self.vel.y = JUMP
 
+   
+
     def update(self):
         self.acc = vec(0,PLAYER_GRAV)
         keys = pg.key.get_pressed()
-        if keys[pg.K_LEFT]:
+        if keys[pg.K_a]:
             self.acc.x = -PLAYER_ACC
-        if keys[pg.K_RIGHT]:
+        if keys[pg.K_d]:
             self.acc.x = PLAYER_ACC
 
 
@@ -40,15 +42,61 @@ class Player(pg.sprite.Sprite):
 
 
         self.rect.midbottom = self.pos
+    
 
 class Bulett(pg.sprite.Sprite):
-    def __init__(self,x,y):
+    def __init__(self,x,y,s,k):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((2,3))
+        self.image = pg.Surface((4,6))
         self.image.fill(BLACK)
-        self.rect = self.image.rect()
+        self.rect = self.image.get_rect()
         self.rect.centery = y
         self.rect.centerx = x
+        self.speedy = s
+        self.key = k
+
+    def update(self):
+        if self.key == 1:
+            self.rect.y += self.speedy
+            if self.rect.bottom < 0:
+                self.kill()
+        else:
+            self.rect.x += self.speedy
+            if self.rect.bottom < 0:
+                self.kill()
+
+
+
+class Mob(pg.sprite.Sprite):
+    def __init__(self,game):
+        self.groups = game.all_sprites, game.mobs
+        pg.sprite.Sprite.__init__(self,self.groups)
+        self.game = game
+        self.image = pg.Surface((20,20))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = random.choice([-10,WIDTH+10])
+        self.vx = random.randrange(3,6)
+        if self.rect.centerx>WIDTH:
+            self.vx *=-1
+        self.rect.y = random.randrange(HEIGHT-60)
+        self.vy =0
+        self.dy = 0.5
+
+    def update(self):
+        self.rect.x += self.vx
+        self.vy += self.dy
+        if self.vy >3 or self.vy <-3:
+            self.dy *=-1
+        center = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.rect.y += self.vy
+        if self.rect.left > WIDTH +100 or self.rect.right <-100:
+            self.kill()
+         
+
+
 
 class Platform(pg.sprite.Sprite):
     def __init__(self,x,y,w,h):
